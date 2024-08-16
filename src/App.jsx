a733186b719d4ee4c3cc2b33c71646872e97de50
VitaliyPritula <pritula.vitaly94@gmail.com>
2024-08-16 16:52:51 +0300

    srart develop

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Content } from './components/layout/Content/content';
+import { Footer } from './components/layout/Footer/Footer';
+import { Header } from './components/layout/Header/Header';
+import './App.scss';
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Content/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
