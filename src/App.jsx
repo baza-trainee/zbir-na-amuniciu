0c4a01254cf5157be3f1de75a41f3e704394e826
NataliaMahera <magera.nataliia@gmail.com>
2024-08-17 10:27:25 +0200

    refactor: change some files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Content } from './components/layout/Content/content';
 import { Footer } from './components/layout/Footer/Footer';
 import { Header } from './components/layout/Header/Header';
-import './App.scss';
+import './styles/index.scss';
 
 function App() {
   return (
