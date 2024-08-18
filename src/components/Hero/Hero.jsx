43b960f4bf6e3464ed0a77284c69c5244396db08
VitaliyPritula <pritula.vitaly94@gmail.com>
2024-08-18 22:52:57 +0300

    crtate header

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+
+const Hero = () => {
+
+    return (
+        <section className="HeroSection">
+            <div className="container">
+
+            </div>
+        </section>
+    );
+};
+export default Hero;
--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -1,5 +1,9 @@
 export const Footer = () => {
-    return (
-       <footer>Footer</footer>
-    );
+  return (
+    <footer className="footer">
+      <div className="container">
+        <p>Розробка Baza Trainee Ukraine 2024 © Всі права захищені</p>
+      </div>
+    </footer>
+  );
 };
