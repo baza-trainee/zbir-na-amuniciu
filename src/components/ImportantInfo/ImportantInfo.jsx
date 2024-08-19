d84a21678b95003635c181c90158f3a51356f654
VladAntonov95 <vladislav.95antonov@gmail.com>
2024-08-19 19:14:01 +0300

    feat: add important info section

---
+++ src/components/ImportantInfo/ImportantInfo.jsx
@@ -0,0 +1,50 @@
+import sprite from '../../assets/icons/sprite.svg';
+import warrior from '../../assets/img/desktop/info-section-warrior.png';
+
+const ImportantInfo = () => {
+  return (
+    <section className="container importan-info-section">
+      <h2 className="important-section-title">ЧОМУ ЦЕ ВАЖЛИВО</h2>
+      <div className="important-section">
+        <div>
+          <ul className="important-section-text">
+            <li className="important-section-list">
+              <svg className="svg-check">
+                <use href={sprite + '#icon-check'}></use>
+              </svg>
+              <p className="important-section-paragraph">
+                Ваш внесок – реальна допомога Воїну, який мужньо захищає нас від
+                ворога
+              </p>
+            </li>
+            <li className="important-section-list">
+              <svg className="svg-check">
+                <use href={sprite + '#icon-check'}></use>
+              </svg>
+              <p className="important-section-paragraph">
+                Якісна амуніція суттєво знижує ризик отримання травм і рятує
+                життя в бойових умовах
+              </p>
+            </li>
+            <li className="important-section-list">
+              <svg className="svg-check">
+                <use href={sprite + '#icon-check'}></use>
+              </svg>
+              <p className="important-section-paragraph">
+                Наявність сучасної амуніції дозволить Олександру краще
+                виконувати свої завдання і наближувати нашу перемогу
+              </p>
+            </li>
+          </ul>
+          <p className="important-section-footer">ЧЕКАЄМО ДОДОМУ!</p>
+        </div>
+        <img
+          src={warrior}
+          alt="warrior-image"
+          className="info-section-warrior"
+        />
+      </div>
+    </section>
+  );
+};
+export default ImportantInfo;
--- src/components/layout/Content/content.jsx
+++ src/components/layout/Content/content.jsx
@@ -1,15 +1,17 @@
-import ProgressBar from "../../ProgressBar/ProgressBar";
-import ProgressBr from "../../Hero/Hero";
-import About from '../About/About';
+import ProgressBar from '../../ProgressBar/ProgressBar';
+import ProgressBr from '../../Hero/Hero';
+import About from '../About/About'; // change placement
+import ImportantInfo from '../../ImportantInfo/ImportantInfo';
 // import { TestComponent } from '../TestComponent/TestComponent';
 
 export const Content = () => {
   return (
     <main>
-      <ProgressBr/>
-      <ProgressBar/>
+      <ProgressBr />
+      <ProgressBar />
       {/* <TestComponent/> */}
       <About />
+      <ImportantInfo />
     </main>
   );
 };
