4793b14a8b393daeba83909986574cade1c56f0e
Baza Trainee Ukraine <132754508+baza-trainee@users.noreply.github.com>
2024-08-20 22:51:29 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import eslint from 'vite-plugin-eslint';
-import stylelint from 'vite-plugin-stylelint';
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import eslint from 'vite-plugin-eslint';
+// import stylelint from 'vite-plugin-stylelint';
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react(), eslint(), stylelint()],
+// })
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import svgr from 'vite-plugin-svgr';
+
 export default defineConfig({
-  plugins: [react(), eslint(), stylelint()],
-})
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      src: '/src',
+      components: '/src/components',
+      pages: '/src/pages',
+      assets: '/src/assets',
+    },
+  },
+  base: '/zbir-na-amuniciu',
+});
