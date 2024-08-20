637a9a48aaa40f5e318607a84a43334cee77a242
VitaliyPritula <pritula.vitaly94@gmail.com>
2024-08-20 12:01:03 +0300

    edin headers

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -9,7 +9,7 @@ export const Header = () => {
 
   const links = [
     { text: 'Мета', sectionId: '1' },
-    { text: 'Нааа що збираєм', sectionId: '2' },
+    { text: 'На що збираєм', sectionId: '2' },
     { text: 'Чому це важливо', sectionId: '3' },
   ];
 
